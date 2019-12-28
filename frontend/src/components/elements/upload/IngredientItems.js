9f653993b57cd9cebd98d8a43cbc786bf6bf475f
Stefan Chan <stefan2846@hotmail.com>
2019-12-28 21:19:39 +0100

    Rename IngredientItem to more appropriate name

---
+++ frontend/src/components/elements/upload/IngredientItems.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { DigitText, DigitTooltip } from "@cthit/react-digit-components";
+import "./IngredientItems.css";
+
+class IngredientItems extends Component {
+  handleDelete = ingredient => {
+    this.props.handleDelete(ingredient);
+  };
+
+  formatText = ingredient => {
+    let ing = ingredient[0];
+    let amount = ingredient[1];
+    let meassurement = ingredient[2];
+
+    return amount + " " + meassurement + " " + ing;
+  };
+
+  render() {
+    return (
+      <ul className="ingredientList">
+        {this.props.ingredients.map(ingredient => {
+          let itemKey = Math.floor(Math.random() * 1000 + 1);
+          return (
+            <DigitTooltip text="Click to delete">
+              <li
+                onClick={() => {
+                  this.handleDelete(ingredient);
+                }}
+                // Generate a random number to use as key
+                key={itemKey}
+              >
+                <DigitText.Text
+                  text={this.formatText(ingredient)}
+                  white={true}
+                />
+              </li>
+            </DigitTooltip>
+          );
+        })}
+      </ul>
+    );
+  }
+}
+
+export default IngredientItems;
